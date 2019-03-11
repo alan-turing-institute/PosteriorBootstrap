d5143a1cc041fd000685c5124fb89b576575d4e9
Miguel Morin <32396311+miguelmorin@users.noreply.github.com>
2019-03-11 12:17:52 +0000

    Add tests for data loading

--- tests/testthat/test_input_checking.R
+++ tests/testthat/test_input_checking.R
@@ -1,11 +1,9 @@
 context("Input checking on the German statlog and the generated toy datasets")
 library(PosteriorBootstrap)
 
+
 test_that("German statlog data outcomes are in {1, 2}", {
-  data_file <- system.file("extdata",
-                           "statlog-german-credit.dat",
-                           package = "PosteriorBootstrap")
-  raw_dataset <- as.matrix(utils::read.table(data_file))
+  raw_dataset <- as.matrix(utils::read.table(k_german_credit_file))
   expect_true(all(raw_dataset[, ncol(raw_dataset)] %in% c(1, 2)))
 })
 
@@ -13,4 +11,19 @@ test_that("Toy dataset outcomes are in {-1, 1}", {
   toy <- load_dataset()
   expect_true(all(toy$y_train %in% c(-1, 1)))
   expect_true(all(toy$y_test %in% c(-1, 1)))
+})
+
+test_that("German statlog data has all expected fields", {
+  toy <- load_dataset()
+  german <- load_dataset(list(name = k_german_credit,
+                              pct_train = 1))
+  expected_names <- c("name", "pct_train",
+                      "n", "n_cov", "n_train", "n_test",
+                      "obs", "x", "y",
+                      "obs_train", "x_train", "y_train",
+                      "obs_test", "x_test", "y_test")
+  expect_equal(length(setdiff(names(toy), expected_names)), 0)
+  expect_equal(length(setdiff(expected_names, names(toy))), 0)
+  expect_equal(length(setdiff(names(german), expected_names)), 0)
+  expect_equal(length(setdiff(expected_names, names(german))), 0)
 })
