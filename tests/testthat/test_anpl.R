38446a7448bfe6a40e2e75330f8de5afbdc9a511
Miguel Morin <32396311+miguelmorin@users.noreply.github.com>
2019-05-20 17:55:04 +0100

    Fixed bug in mcmapply and added test for it

--- tests/testthat/test_anpl.R
+++ tests/testthat/test_anpl.R
@@ -1,8 +1,9 @@
 context("Adaptive non-parametric learning function")
-library(PosteriorBootstrap)
 library(parallel)
+library(PosteriorBootstrap)
+library(rstan)
 
-test_that("Mixture of Dirichlet Processes stick-breaking works and returns", {
+test_that("Adaptive non-parametric learning with centering model works", {
 
   german <- load_dataset(list(name = k_german_credit))
 
@@ -17,6 +18,15 @@ test_that("Mixture of Dirichlet Processes stick-breaking works and returns", {
 
   expect_true(is.numeric(anpl_samples))
   expect_equal(dim(anpl_samples), c(n_bootstrap, 1 + german$n_cov))
+
+  # The following two tests relate to using the result of `mcmapply`. If it's
+  # used as a list (like the result of `mclapply`) instead of a matrix, either
+  # the columns or the rows will all be the same.
+  expect_true(all(diag(var(anpl_samples)) > 0),
+              "Row values in ANPL samples are different")
+  expect_true(all(diag(var(t(anpl_samples))) > 0),
+              "Column values in ANPL samples are different")
+
 })
 
 test_that("Multiple processors are available", {
@@ -68,4 +78,47 @@ test_that("Parallelisation works and is faster", {
   }
   expect_true(speedup > expected_speedup,
               "Largest parallelization speedup is larger than 70%")
+})
+
+test_that("Adaptive non-parametric learning with posterior samples works well", {
+
+  german <- load_dataset(list(name = k_german_credit))
+  n_bootstrap <- 100
+
+  # Get posterior samples
+  prior_variance <- 100
+  bayes_logit_model <- rstan::stan_model(file = get_rstan_file())
+  train_dat <- list(n = german$n,
+                    p = german$n_cov + 1,
+                    x = cbind(1, german$x),
+                    y = 0.5 * (german$y + 1),
+                    beta_sd = sqrt(prior_variance))
+
+  set.seed(1)
+  stan_vb <- rstan::vb(bayes_logit_model,
+                       data = train_dat,
+                       output_samples = n_bootstrap,
+                       seed = 123,
+                       iter = 100)
+  stan_vb_sample <- rstan::extract(stan_vb)$beta
+
+  # Use these samples in ANPL with multiple cores
+  anpl_samples <- anpl(dataset = german,
+                       concentration = 1,
+                       n_bootstrap = n_bootstrap,
+                       posterior_sample = stan_vb_sample,
+                       threshold = 1e-8,
+                       num_cores = 2)
+  
+  # Once we got a problem with coefficients way off because of misuse of
+  # `mcmapply` (as if it were a list instead of a matrix). So we added a test
+  # for the average value of the coefficients in the paper for the specific
+  # magic numbers in this tests, where reproducibility ensues from `set.seed()`.
+  col_means <- colMeans(anpl_samples)
+  print(col_means[21])
+  print(col_means[22])
+  expect_true((col_means[21] >= 0.2) && (col_means[21] <= 0.4),
+              "The average coefficient for column 21 is as expected")
+  expect_true((col_means[22] >= -0.3) && (col_means[22] <= -0),
+              "The average coefficient for column 22 is as expected")
 })
