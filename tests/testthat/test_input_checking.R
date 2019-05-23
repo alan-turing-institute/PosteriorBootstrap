67b5b02c33dedaf1668c69d1f22ca784dffe5822
Miguel Morin <32396311+miguelmorin@users.noreply.github.com>
2019-05-23 12:32:43 +0100

    Removed toy dataset

--- tests/testthat/test_input_checking.R
+++ tests/testthat/test_input_checking.R
@@ -7,33 +7,15 @@ test_that("German statlog data outcomes are in {1, 2}", {
   expect_true(all(raw_dataset[, ncol(raw_dataset)] %in% c(1, 2)))
 })
 
-test_that("Toy dataset outcomes are in {-1, 1}", {
-  toy <- load_dataset()
-  expect_true(all(toy$y_train %in% c(-1, 1)))
-  expect_true(all(toy$y_test %in% c(-1, 1)))
-})
-
-names_helper <- function(dataset) {
-  expected_names <- c("name", "n", "n_cov", "x", "y")
-  expect_equal(length(setdiff(names(dataset), expected_names)), 0)
-  expect_equal(length(setdiff(expected_names, names(dataset))), 0)
-}
-
 test_that("German statlog and toy data have all expected fields", {
-  toy <- load_dataset()
-  german <- load_dataset(list(name = k_german_credit))
-  names_helper(toy)
-  names_helper(german)
+  german <- load_dataset()
+  expected_names <- c("n", "n_cov", "x", "y")
+  expect_equal(length(setdiff(names(german), expected_names)), 0)
+  expect_equal(length(setdiff(expected_names, names(german))), 0)
 })
-
-consistency_helper <- function(dataset) {
-  expect_equal(dim(dataset$x), c(dataset$n, dataset$n_cov))
-  expect_true(all(dataset$y %in% c(-1, 1)))
-}
 
 test_that("German statlog and toy data fields are consistent", {
-  toy <- load_dataset()
-  german <- load_dataset(list(name = k_german_credit))
-  consistency_helper(toy)
-  consistency_helper(german)
+  german <- load_dataset()
+  expect_equal(dim(german$x), c(german$n, german$n_cov))
+  expect_true(all(german$y %in% c(-1, 1)))
 })
