146013921dd96d000b25ab88cd9a95d21920e2f0
Miguel Morin <32396311+miguelmorin@users.noreply.github.com>
2019-05-23 21:31:30 +0100

    Tested length of output of stick-breaking as multiple of input

--- tests/testthat/test_stick_breaking.R
+++ tests/testthat/test_stick_breaking.R
@@ -6,3 +6,12 @@ test_that("Stick-breaking function adds up to one", {
   weights <- stick_breaking()
   expect_equal(sum(weights), 1)
 })
+
+test_that("Stick-breaking output has a multiple size of input", {
+  # Such a multiple is required in `anpl_single`, where `x_prior` needs to be stacked
+  # collated multiple times so the number of columns equals the number of stick breaks
+  for (min_stick_breaks in seq(2, 20)) {
+    weights <- stick_breaking(min_stick_breaks = min_stick_breaks)
+    expect_equal(0, length(weights) %% min_stick_breaks)
+  }
+})
