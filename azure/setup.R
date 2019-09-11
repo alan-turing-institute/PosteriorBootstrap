312523285822b83926ad3c1e677fbdbefcea487f
Miguel Morin <32396311+miguelmorin@users.noreply.github.com>
2019-09-11 20:33:37 +0100

    Added last changes for Rstan problem and made change to fix StanHeaders azure problem

--- azure/setup.R
+++ azure/setup.R
@@ -3,7 +3,7 @@ dir.create(lib)
 .libPaths(c(lib, .libPaths()))
 options(repos=structure(c(CRAN = "https://cran.r-project.org")))
 packages <- c("devtools", "dplyr", "e1071", "ggplot2", "gridExtra", "MASS", "Rcpp",
-              "rstan", "utils", "tibble", "knitr", "lintr", "rmarkdown",
+              "StanHeaders", "rstan", "utils", "tibble", "knitr", "lintr", "rmarkdown",
               "roxygen2", "testthat")
 for (p in packages) {
   install.packages(p)
