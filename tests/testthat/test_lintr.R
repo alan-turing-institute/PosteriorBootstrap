e6747fd62f3672d23bc90f732643ed9e87f0a490
James Robinson <james.em.robinson@gmail.com>
2021-05-11 20:00:29 +0100

    Added lintr to installation list

--- tests/testthat/test_lintr.R
+++ tests/testthat/test_lintr.R
@@ -1,6 +1,7 @@
-context("Code style errors")
-requireNamespace("lintr", quietly = TRUE)
+if (requireNamespace("lintr", quietly = TRUE)) {
+  context("Code style errors")
 
-test_that("Package has no lintr errors", {
-  lintr::expect_lint_free()
-})
+  test_that("Package has no lintr errors", {
+    lintr::expect_lint_free()
+  })
+}
